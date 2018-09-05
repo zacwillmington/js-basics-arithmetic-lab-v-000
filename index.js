3d20a5ee77a3feb9e620d04781b9a0a13e861e0c
Zac Willmington <zac.willmington@gmail.com>
2018-09-05 16:06:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*
-Variables defined in the test suite:
-- oldID
-- currentAge
-
-You should NOT try to redefine any of the above variables.
-*/
-
-// Write your code below this line!
 let newID = 1000000000 + oldID;
 
 let ageIsValid = Number.isInteger(currentAgeIsInteger);
-
-// let randomNumber = Math.floor(Math.ramdom(1..20) * 10); 
