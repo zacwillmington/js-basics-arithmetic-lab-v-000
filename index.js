35eee1de60d95bc38c661c90de3cf80f6afcbaa6
Zac Willmington <zac.willmington@gmail.com>
2018-09-05 16:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let newID = 1000000000 + oldID;
 let ageIsValid = Number.isInteger(currentAgeIsInteger);
 
 let randomNumber = Math.round(Math.random() * 20);
+
+
+let randomInteger = Math.floor(Math.random() * 100);
